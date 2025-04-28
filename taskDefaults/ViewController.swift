c87aebf651c578269b5f20687e8583faeb25cac9
felipesantos22 <felipevs2018@gmail.com>
2025-04-28 11:56:23 -0300

    first commit

---
+++ taskDefaults/ViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  taskDefaults
+//
+//  Created by Felipe Santos on 28/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var tasks : [String] = []
+    
+    let textField : UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Adicionar tarefas..."
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let button : UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("+", for: .normal)
+        button.layer.borderColor = UIColor.black.cgColor
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let tableView : UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    func saveTasks() {
+        UserDefaults.standard.set(tasks, forKey: "tasks")
+    }
+    
+    @objc func addTask() {
+        if let task = textField.text, !task.isEmpty {
+            tasks.append(task)
+            saveTasks()
+            tableView.reloadData()
+            textField.text = ""
+        }
+    }
+    
+    func loadTasks() -> [String] {
+        return UserDefaults.standard.stringArray(forKey: "tasks") ?? []
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func configureConstraints() {
+        [textField, button, tableView].forEach {
+            view.addSubview($0)
+        }
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            textField.trailingAnchor.constraint(equalTo: button.leadingAnchor, constant: -10),
+            textField.heightAnchor.constraint(equalToConstant: 30),
+            
+            button.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            button.heightAnchor.constraint(equalToConstant: 30),
+            
+            tableView.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 10),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        button.addTarget(self, action: #selector(addTask), for: .touchUpInside)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        configureConstraints()
+        tasks = loadTasks()
+    }
+    
+}
+
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+    
+    
+}
+
